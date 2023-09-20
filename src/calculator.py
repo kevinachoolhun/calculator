5afd18f39180dfabea4a0ea8eb2086790ea56162
kevinachoolhun <kevina.choolhun@gmail.com>
2023-09-20 23:06:22 +0400

    init.py to create package

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,14 @@
 
 def add(a, b):
+    """Compute and return the sum of two numbers.
+    
+    Args:
+        a(float): A number representing the first addend in the addition
+        b(float): A number representing thee second addend in the addition. 
+        
+    Returns:
+        float: A number representing the arithmetic sum of 'a' and 'b'.
+    """
     return float(a + b)
 
 def subtract(a, b):
